32038fabec3d03e5448c3c55c14f709e4b6a34c5
Gaston Mousques <mousques@hotmail.com>
2018-05-29 13:59:24 -0300

    se agrego manejo de servo y se comento para mayor claridad

--- thingsBoardPasoaPaso.ino
+++ thingsBoardPasoaPaso.ino
@@ -9,23 +9,27 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 
-// includes de bibliotecas sensores
-#include "DHT.h"
 
-//  configuración datos wifi
+//***************MODIFICAR PARA SU PROYECTO *********************
+// includes de bibliotecas sensores, poner las que usen en este caso un DHT11 y un Servo
+#include "DHT.h"
+#include <Servo.h>
+
+
+//***************MODIFICAR PARA SU PROYECTO *********************
+//  configuración datos wifi 
+// decomentar el define y poner los valores de su red y de su dispositivo
 //#define WIFI_AP "SSID RED"
 //#define WIFI_PASSWORD "PASSWORD RED"
 
 
-
-
 //  configuración datos thingsboard
-//#define NODE_ID "NOMBRE DISPOSITIVO"
-//#define NODE_TOKEN "TOKEN DISPOSITIVO"
+//#define NODE_NAME "NOMBRE DISPOSITIVO"   //nombre que le pusieron al dispositivo cuando lo crearon
+//#define NODE_TOKEN "TOKEN DISPOSITIVO"   //Token que genera Thingboard para dispositivo cuando lo crearon
 
 
 
-
+//***************NO MODIFICAR *********************
 char thingsboardServer[] = "demo.thingsboard.io";
 
 /*definir topicos.
@@ -37,10 +41,6 @@ char telemetryTopic[] = "v1/devices/me/telemetry";
 char requestTopic[] = "v1/devices/me/rpc/request/+";  //RPC - El Servidor usa este topico para enviar rquests, cliente response
 char attributesTopic[] = "v1/devices/me/attributes";  // Permite recibir o enviar mensajes dependindo de atributos compartidos
 
-// configuración sensores
-#define DHTPIN D1
-#define DHTTYPE DHT11
-
 
 // declarar cliente Wifi y PubSus
 WiFiClient wifiClient;
@@ -50,25 +50,43 @@ PubSubClient client(wifiClient);
 unsigned long lastSend;
 const int elapsedTime = 1000; // tiempo transcurrido entre envios al servidor
 
+
+//***************MODIFICAR PARA SU PROYECTO *********************
+// configuración sensores que utilizan
+#define DHTPIN D1
+#define DHTTYPE DHT11
+
+
 // Declarar e Inicializar sensores.
 DHT dht(DHTPIN, DHTTYPE);
 
+Servo doorServo;  // create servo object to control a servo 
+const int servoDoorClosedPosition = 0;
+const int servoDoorOpenedPosition = 90;
+const int servoPin = D4;
 
+//************************* Funciones *********************
 // función setup micro
 void setup()
 {
   Serial.begin(9600);
-  dht.begin();
-  delay(10);
 
   // inicializar wifi y pubsus
-  connetToWiFi();
+  connectToWiFi();
   client.setServer( thingsboardServer, 1883 );
 
   // agregado para recibir callbacks
   client.setCallback(on_message);
    
   lastSend = 0; // para controlar cada cuanto tiempo se envian datos
+
+  // AGREGAR IINICIALZICION DE SENSORES PARA SU PROOYECTO
+  doorServo.attach(servoPin); 
+
+  initDoor();  // cierra la puerta
+  
+  dht.begin(); //inicaliza el DHT
+  delay(10);
 }
 
 // función loop micro
@@ -80,18 +98,23 @@ void loop()
 
   if ( millis() - lastSend > elapsedTime ) { // Update and send only after 1 seconds
     // Enviar datos de telemetria
-    getAndSendData();
+    getAndSendData();   // FUNCION QUE ENVIA INFORMACIÓN DE TELEMETRIA
     lastSend = millis();
   }
 
   client.loop();
 }
 
+//***************MODIFICAR PARA SU PROYECTO *********************
 /*
  * función para leer datos de sensores y enviar telementria al servidor
+ * Se debe sensar y armar el JSON especifico para su proyecto
  */
 void getAndSendData()
 {
+
+  // Lectura de sensores
+  
   Serial.println("Collecting temperature data.");
 
   // Reading temperature or humidity takes about 250 milliseconds!
@@ -116,19 +139,23 @@ void getAndSendData()
   String humidity = String(h);
 
 
-  // Just debug messages
+  //Debug messages
   Serial.print( "Sending temperature and humidity : [" );
   Serial.print( temperature ); Serial.print( "," );
   Serial.print( humidity );
   Serial.print( "]   -> " );
 
-  // Prepare a JSON payload string
+
+  // Preparar el payload del JSON payload
+  // el formato es {key"value, Key: value, .....}
+  // en este caso seria {"tetemperature:"valor, "humidity:"valor}
+  //
   String payload = "{";
   payload += "\"temperature\":"; payload += temperature; payload += ",";
   payload += "\"humidity\":"; payload += humidity;
   payload += "}";
 
-  // Send payload
+  // Enviar el payload al servidor usando el topico para telemetria
   char attributes[100];
   payload.toCharArray( attributes, 100 );
   if (client.publish( telemetryTopic, attributes ) == true)
@@ -138,9 +165,14 @@ void getAndSendData()
 
 }
 
+
+//***************MODIFICAR PARA SU PROYECTO *********************
 /* 
  *  Este callback se llaman cuando se utilizan widgets de control que envian mensajes por el topico requestTopic
  *  en la función de reconnect se realiza la suscripción al topico de request
+ *  el Topico por el cual llegan los comandos viene en el parametro topci, en este ejemplo es requestTopic 
+ *  El formato del string que llega es "v1/devices/me/rpc/request/{id}/Operación. donde operación es el método get declarado en el  
+ *  widget que usan para mandar el comando e {id} es el indetificador del mensaje generado por el servidor
  */
 void on_message(const char* topic, byte* payload, unsigned int length) 
 {
@@ -172,7 +204,11 @@ void on_message(const char* topic, byte* payload, unsigned int length)
   Serial.print("Nombre metodo:");
   Serial.println(methodName);
 
-  //responder segun el método 
+  /* Responder segun el método 
+   *  En este caso 
+   *     el widget switch envia el comando openDoor con un paramtro true o false
+   *     el wisget gauge envian un comando rotateMotorValue con un int de los grados a rotar el motos (servo)
+   */
   if (methodName.equals("openDoor")) {
     bool action = data["params"];
     String doorStatus = openDoor(action);
@@ -193,27 +229,40 @@ void on_message(const char* topic, byte* payload, unsigned int length)
  
 }
 
+//***************MODIFICAR PARA SU PROYECTO *********************
+
 /*
- * función que "abre" la puerta (simulada)
+ * función que "abre" la puerta (simulada).
+ * En su proyecto habria que programar lo que hace el dispositivo al recibir el comando
  */
 String openDoor(bool action)
 {
   String returnValue = "NO ANDA";
   if (action == true) {
+    doorServo.write(servoDoorOpenedPosition);
     Serial.println("Abriendo puerta");
     returnValue = "ABIERTA";
   }
    else {
+    doorServo.write(servoDoorClosedPosition);
     Serial.println("Cerrando puerta");
     returnValue = "CERRADA";
    }
    return returnValue;
 }
 
+// Función para inicializar con la puerta cerrada
+void initDoor()
+{
+  doorServo.write(servoDoorClosedPosition);
+}
+
 /*
  * 
- *Reply al servidro del estado de la puerta. se modifica el atributo compartido doorState y se refleja en la card asociada al misimo
- *
+ *Reply al servidor del estado de la puerta. se modifica el atributo compartido doorState y se refleja en la card asociada al misimo
+ *Las respuestas al servidor son OPCIONALES.   El formato del string que se envia es "v1/devices/me/rpc/response/{id}/Respuesta. donde Respuesta es el método get declarado en el  
+ *widget que usan para mandar el comando e {id} es el indetificador del mensaje generado por el servidor. Notar que en el string del nomber del tópico 
+ *se sustituye la palabra Resquest por Response, esto se hace para reusar el nombre del topico que solo difiere en si es request o response
  */
 void replyDoorRequest(String doorStatus, const char* topic)
 {
@@ -226,6 +275,7 @@ void replyDoorRequest(String doorStatus, const char* topic)
      String payload = "{";
      payload += "\"doorState\":"; payload += "\""  ; payload += doorStatus; payload += "\""; payload += "}";
 
+
     // Send payload
     char attributes[100];
     payload.toCharArray( attributes, 100 );
@@ -236,13 +286,16 @@ void replyDoorRequest(String doorStatus, const char* topic)
     client.publish(attributesTopic, attributes);
 }
 
-/*
+
+//***************MODIFICAR PARA SU PROYECTO *********************
+/* El proceso para otros comandos es el mismo que en el caso de openDoor
  * función que gira un servo X grados (simulada)
  */
 void moverMotor(int grados)
 {
   Serial.print("moviendo motor");
   Serial.println(grados);
+  doorServo.write(grados);
 }
 
 /*
@@ -281,7 +334,7 @@ void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     statusWifi = WiFi.status();
-    connetToWiFi();
+    connectToWiFi();
     
     Serial.print("Connecting to ThingsBoard node ...");
     // Attempt to connect (clientId, username, password)
@@ -304,7 +357,7 @@ void reconnect() {
 /*
  * función para conectarse a wifi
  */
-void connetToWiFi()
+void connectToWiFi()
 {
   Serial.println("Connecting to WiFi ...");
   // attempt to connect to WiFi network
